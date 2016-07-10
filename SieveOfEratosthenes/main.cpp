560f778f9c410b471d0908e51c07850e2f056ce7
Brent Perry <burentoperii@gmail.com>
2016-07-10 14:59:54 -0500

    Initial commit of SieveOfEratosthenes.

--- SieveOfEratosthenes/main.cpp
+++ SieveOfEratosthenes/main.cpp
@@ -6,10 +6,33 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    vector<int> master_list;
+    
+    master_list.push_back(0);
+    master_list.push_back(0);
+    
+    for (int i = 2; i <= 100; ++i) {
+        master_list.push_back(1);
+    }
+    
+    for (int i = 2; i <= sqrt(100); ++i) {
+        if (master_list[i] == 1) {
+            for (int j = i * 2; j <= 100; j+=i) {
+                master_list[j] = 0;
+            }
+        }
+    }
+    
+    cout << "Prime numbers found: \n";
+    for (int i = 0; i < master_list.size(); ++i) {
+        if (master_list[i] == 1) {
+            cout << i << " ";
+        }
+    }
+    cout << "\n";
+    
 }
