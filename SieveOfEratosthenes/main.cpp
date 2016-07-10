e4f7123c581a6111d21e7abd0765c686403915bd
Brent Perry <burentoperii@gmail.com>
2016-07-10 15:51:03 -0500

    Add ability for user to input the upper_max of the range.

--- SieveOfEratosthenes/main.cpp
+++ SieveOfEratosthenes/main.cpp
@@ -11,23 +11,28 @@
 int main()
 {
     vector<int> master_list;
+    int user_max;
+    
+    cout << "Please enter a number to be the upper max: ";
+    cin >> user_max;
+    cout << "\n";
     
     master_list.push_back(0);
     master_list.push_back(0);
     
-    for (int i = 2; i <= 100; ++i) {
+    for (int i = 2; i <= user_max; ++i) {
         master_list.push_back(1);
     }
     
-    for (int i = 2; i <= sqrt(100); ++i) {
+    for (int i = 2; i <= sqrt(user_max); ++i) {
         if (master_list[i] == 1) {
-            for (int j = i * 2; j <= 100; j+=i) {
+            for (int j = i * 2; j <= user_max; j+=i) {
                 master_list[j] = 0;
             }
         }
     }
     
-    cout << "Prime numbers found: \n";
+    cout << "Prime numbers found up to your max: \n";
     for (int i = 0; i < master_list.size(); ++i) {
         if (master_list[i] == 1) {
             cout << i << " ";
